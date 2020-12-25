dd8cb845fdda916c1ec349e5c812c4be97bd522c
Ondrej Kupka <ondra.cap@gmail.com>
2020-12-25 16:24:40 +0100

    wip
    [ci skip]

--- adapter.go
+++ adapter.go
@@ -10,6 +10,9 @@ type Adapter struct{}
 func (adapter *Adapter) LoadPolicy(model model.Model) error {
 }
 
+// LoadFilteredPolicy loads only policy rules that match the filter.
+func (adapter *Adapter) LoadFilteredPolicy(model model.Model, filter interface{}) error {}
+
 // SavePolicy saves all policy rules to the storage.
 func (adapter *Adapter) SavePolicy(model model.Model) error {}
 
@@ -18,26 +21,27 @@ func (adapter *Adapter) SavePolicy(model model.Model) error {}
 func (adapter *Adapter) AddPolicy(sec string, ptype string, rule []string) error {
 }
 
+// AddPolicies adds policy rules to the storage.
+// This is part of the Auto-Save feature.
+func (adapter *Adapter) AddPolicies(sec string, ptype string, rules [][]string) error {}
+
 // RemovePolicy removes a policy rule from the storage.
 // This is part of the Auto-Save feature.
 func (adapter *Adapter) RemovePolicy(sec string, ptype string, rule []string) error {
 }
+
+// RemovePolicies removes policy rules from the storage.
+// This is part of the Auto-Save feature.
+func (adapter *Adapter) RemovePolicies(sec string, ptype string, rules [][]string) error {}
 
 // RemoveFilteredPolicy removes policy rules that match the filter from the storage.
 // This is part of the Auto-Save feature.
 func (adapter *Adapter) RemoveFilteredPolicy(sec string, ptype string, fieldIndex int, fieldValues ...string) error {
 }
 
-// AddPolicies adds policy rules to the storage.
-// This is part of the Auto-Save feature.
-func (adapter *Adapter) AddPolicies(sec string, ptype string, rules [][]string) error {}
-
-// RemovePolicies removes policy rules from the storage.
-// This is part of the Auto-Save feature.
-func (adapter *Adapter) RemovePolicies(sec string, ptype string, rules [][]string) error {}
-
-// LoadFilteredPolicy loads only policy rules that match the filter.
-func (adapter *Adapter) LoadFilteredPolicy(model model.Model, filter interface{}) error {}
-
 // IsFiltered returns true if the loaded policy has been filtered.
 func (adapter *Adapter) IsFiltered() bool {}
+
+// UpdatePolicy updates a policy rule from storage.
+// This is part of the Auto-Save feature.
+func (adapter *Adapter) UpdatePolicy(sec string, ptype string, oldRule, newPolicy []string) error {}
