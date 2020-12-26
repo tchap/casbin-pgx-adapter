c3d998daa074cd59be84b9b01df0665d58593797
Ondrej Kupka <ondra.cap@gmail.com>
2020-12-26 18:26:05 +0100

    wip
    [ci skip]

--- policy.go
+++ policy.go
@@ -6,8 +6,10 @@ import (
 	"github.com/casbin/casbin/v2/model"
 )
 
+// loadPolicyLine is based on https://pkg.go.dev/github.com/casbin/casbin/v2/persist#LoadPolicyLine
 func loadPolicyLine(r *rule, m model.Model) {
-	key := r.PType.String
+	tokens := r.Tokens()
+	key := tokens[0]
 	sec := key[:1]
 	m[sec][key].Policy = append(m[sec][key].Policy, tokens[1:])
 	m[sec][key].PolicyMap[strings.Join(tokens[1:], model.DefaultSep)] = len(m[sec][key].Policy) - 1
--- query.go
+++ query.go
@@ -9,7 +9,7 @@ import (
 )
 
 type queryExecutor interface {
-	Exec(ctx context.Context, sql string, args ...interface{}) (commandTag pgconn.CommandTag, err error)
+	Exec(ctx context.Context, sql string, args ...interface{}) (pgconn.CommandTag, error)
 	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
 }
@@ -26,6 +26,56 @@ type rule struct {
 	V3    pgtype.Text
 	V4    pgtype.Text
 	V5    pgtype.Text
+}
+
+func (r *rule) Tokens() []string {
+	// V0 and V1 are always present, right?
+	switch {
+	case r.V2.Status == pgtype.Null:
+		return []string{
+			r.PType.String,
+			r.V0.String,
+			r.V1.String,
+		}
+
+	case r.V3.Status == pgtype.Null:
+		return []string{
+			r.PType.String,
+			r.V0.String,
+			r.V1.String,
+			r.V2.String,
+		}
+
+	case r.V4.Status == pgtype.Null:
+		return []string{
+			r.PType.String,
+			r.V0.String,
+			r.V1.String,
+			r.V2.String,
+			r.V3.String,
+		}
+
+	case r.V5.Status == pgtype.Null:
+		return []string{
+			r.PType.String,
+			r.V0.String,
+			r.V1.String,
+			r.V2.String,
+			r.V3.String,
+			r.V4.String,
+		}
+
+	default:
+		return []string{
+			r.PType.String,
+			r.V0.String,
+			r.V1.String,
+			r.V2.String,
+			r.V3.String,
+			r.V4.String,
+			r.V5.String,
+		}
+	}
 }
 
 func listRules(qe queryExecutor) ([]rule, error) {
